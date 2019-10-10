714316ea86517860fce42aaf38f5f490625d486c
Sher-Chowdhury <sherc1980@yahoo.com>
2019-10-10 20:29:02 +0100

    wip

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sher-Chowdhury/gsg_binary_that_uses_cli_parameters.git
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+// need to first run: go get github.com/fatih/color 
+import (
+  "io"
+  "os"
+)
+
+func main() {
+  myMessage := ""
+  arguments := os.Args
+  // https://golang.org/pkg/builtin/#len
+  if len(arguments) == 1 {
+    myMessage = "No arguements provided"
+  } else {
+    myMessage = arguments[1]
+  }
+
+  // https://golang.org/pkg/io
+  // io.WriteString() works in a similar way to fmt.Print(). the main difference is that fmt.Print() can take multiple parameters, e.g. fmt.Print(var1, "some stuff", var2, "even more stuff", "etc") 
+  // wheerase io.WriteString() can only take exactly 2 params, where to write to, and what to write.  
+  io.WriteString(os.Stdout, myMessage)
+
+}
