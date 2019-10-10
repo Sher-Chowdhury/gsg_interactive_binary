5dd933b95cbe8c78afed4f6bef1b1029797fb36a
Sher-Chowdhury <sherc1980@yahoo.com>
2019-10-10 20:31:31 +0100

    wip

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # gsg_binary_that_uses_cli_parameters
+
+Notice here that we created a binary that requires parameters.
+
+
+```
+$ go run .
+No arguements provided
+$ go run . hello
+hello
+$ go run . hello world
+hello
+
+
+
+$ go build .
+$ ./gsg_binary_that_uses_cli_parameters.git
+No arguements provided
+$ ./gsg_binary_that_uses_cli_parameters.git hello
+hello
+```
--- main.go
+++ main.go
@@ -20,5 +20,6 @@ func main() {
   // io.WriteString() works in a similar way to fmt.Print(). the main difference is that fmt.Print() can take multiple parameters, e.g. fmt.Print(var1, "some stuff", var2, "even more stuff", "etc") 
   // wheerase io.WriteString() can only take exactly 2 params, where to write to, and what to write.  
   io.WriteString(os.Stdout, myMessage)
+  io.WriteString(os.Stdout, "\n")
 
 }
