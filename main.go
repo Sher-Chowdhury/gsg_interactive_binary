0bc9be8ea951a258f45c31adf844f30afe1e9e8e
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-10-11 11:56:37 +0100

    wip

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 	// os.File is a path to a file on the fileystem
 	f = os.Stdin // here we chose the Stdin as the the filepath to use. In Linux, stdin, stdout, and stderr are all essentially files behind the scenese
 
+	defer fmt.Println("\nYou did ctrl+d, so exitiing now")
 	defer f.Close() // https://tour.golang.org/flowcontrol/12
 	// defer means that this line is triggered once the main function finishes running.
 	// we are applying the Close method for the f object.
@@ -20,13 +21,14 @@ func main() {
 	// This is a bit like opening the file to start writing to it
 	scanner := bufio.NewScanner(f)
 
+	fmt.Print("Please enter an input: ")
+
 	// https://golang.org/pkg/bufio/#Scanner.Scan
 	// xxx..Scan() is effectively the bash equivalent of of the 'read' command
 	for scanner.Scan() {
 		fmt.Println(reflect.TypeOf(scanner.Text()))
 		fmt.Println(">>>", scanner.Text())
-        }
-  
-  // you need to do ctrl+d to escape this for-loop
+		fmt.Print("Please enter another input: ")
+	}
 
 }
